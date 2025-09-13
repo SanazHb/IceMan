2372a5490837a8399df0ca6c4b48b65da17070e0
SanazHb <Sanaz.habibollahi398@gmail.com>
2025-09-13 11:29:20 +0330

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Enter the degree:");
     let mut Temp = String::new();
-    std::io::stdin().read_line(&mut deg);
+    std::io::stdin().read_line(&mut Temp);
     let float_Temp: f64 = Temp.trim().parse().expect("ERROR");
     if float_Temp > -273.0 && float_Temp <= 6000.0 {
         if float_Temp >= 100.0 {
